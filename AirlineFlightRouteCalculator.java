60f367f9bdc8fa2ca3645efae8691077e3a163b2
houghno <houghno@whs.school.nz>
2021-08-13 10:59:10 +1200

    SCHOOL DESKTOP - cleaning up of commented code as well as added commenting

--- AirlineFlightRouteCalculator.java
+++ AirlineFlightRouteCalculator.java
@@ -3,9 +3,14 @@ import java.io.File;//importing the ability to read information from files, used
 import java.io.IOException;//importing the utilisation of the exception error catching message
 import java.text.*;//importing for decFormat
 /**
+ * Description of whole program:
  * Program in which users select two airports to fly between and the distance and time taken for the route is calculated accordingly
+ * 
+ * Class: 
+ * Runs constructor and contains file reading, Airport selecting and airport name printing methods
  * Note: Unit of distance is a made up latitude x longitude measurement
  * Note: Program cannot calculate the 
+ * 
  * @author Noah Houghton 
  */
 public class AirlineFlightRouteCalculator
@@ -16,7 +21,7 @@ public class AirlineFlightRouteCalculator
     private String selectedAirport = "NO AIRPORT SELECTED";//error message in case no airport was somehow entered
     private int previousAirportSelection;//to store the departure airport and check that the input arrival airport was not the same as this
     private int airportNumber = 0;//counter to allocate an airport to an array index
-    public AirlineFlightRouteCalculator(){//constructor class
+    public AirlineFlightRouteCalculator(){//constructor method
         System.out.print("\f");//clears dialog box
         fileReader();//reads data out from the file of airports and sorts it into the airportDatabase variable
         returnAirports();//returns the names of all Airport variables
@@ -33,45 +38,45 @@ public class AirlineFlightRouteCalculator
 
     public void fileReader(){//reads from file and splits up information within the different variable lines
         File airports=new File("airportdatabase.txt");//creates file airport variable and designates it to the airportdatabase.txt file within the project folder
-        try {
+        try {//tries to run a block of code
             Scanner readAirportsFile = new Scanner(airports);//designates new scanner to read the airport file variable
-            while (readAirportsFile.hasNextLine()){
-                double partsDoubleOne;
-                double partsDoubleTwo;
-                String airportsFileLine=readAirportsFile.nextLine();
-                String splitAtComma=",";
-                String parts[]=airportsFileLine.split(splitAtComma);
-                partsDoubleOne=Double.parseDouble(parts[1]);
-                partsDoubleTwo=Double.parseDouble(parts[2]);
+            while (readAirportsFile.hasNextLine()){//to run the scanner for each line of code
+                double partsDoubleOne;//to keep a commonality of the double variable type within creating airports from the split airport text file
+                double partsDoubleTwo;//to keep a commonality of the double variable type within creating airports from the split airport text file
+                String airportsFileLine=readAirportsFile.nextLine();//gives access to the next line read of the airport file variable and reads it to the string
+                String splitAtComma=",";//splits each line of data in the database file at a ',' creating sections of the line for different properties of the airport 
+                String parts[]=airportsFileLine.split(splitAtComma);//array  using the split "," 
+                partsDoubleOne=Double.parseDouble(parts[1]);//sets the double to the second element of the split array
+                partsDoubleTwo=Double.parseDouble(parts[2]);//sets the double to the third element of the split array
                 airportDatabase[airportNumber]= new Airport(parts[0],partsDoubleOne,partsDoubleTwo);
-                airportNumber++;
+                airportNumber++;//indexes the number of airports created each time the while loop runs
             }
 
         }   
-        catch(IOException e){//catch
-            e.printStackTrace();
+        catch(IOException error){//catch if an exception error appears
+            error.printStackTrace();//prints out details of the error
         }
 
     }
 
-    public int PickAirport(boolean askForDeparture){
-        String pickedAirport;
-        int airportIndex=-1;
-        Scanner airportInput = new Scanner(System.in);
-        boolean airportConfirmation=false;
+    public int PickAirport(boolean askForDeparture/*to check whether the airport being asked should be a departure or arrival airport*/){//prompts user for departure and arrival airports enroute
+        String pickedAirport;//used to compare the input to the static database name.
+        int airportIndex=-1;//set at -1 as the method does not contain an ability to set airportIndex to this value so it checks to see if the program was run
+        Scanner airportInput = new Scanner(System.in);//new scanner that is used for both departure and arrival
+        boolean airportConfirmation=false;//checking if the current airport instance is confirmed by the user
         while(airportConfirmation==false){
             airportIndex=-1;/*counter that changes when an airport is found in the database that has the same name as the user input airport. If i is -1 once the for loop within this class is
             run, it is known that the for loop did not find a valid airport due to the for loop not being able to make i=-1 as it runs from i=0 then i++, each run of the for loop. Therefore it is 
             clear to assume that the airport is invalid*/
-            if(askForDeparture){
+            if(askForDeparture){//runs once for departure as boolena askForDeparture is changed to false when in completes the whole block of code successfully
                 System.out.println("Enter a valid departure airport from the list of availiable airports.");
             }else{
                 System.out.println("Enter a valid arrival airport from the list of availiable airports");
             }
-            pickedAirport=airportInput.nextLine();
+            pickedAirport=airportInput.nextLine();//the input of the user is temporarily stored 
             for (int i = 0;i<airportNumber;i++){//runs for the length of the airportDatabase array
                 if (airportDatabase[i].airportName.equals(pickedAirport.toLowerCase())){
-                    if(askForDeparture){
+                    if(askForDeparture){//runs departure focused block of code
                         selectedAirport = airportDatabase[i].airportName;
                         airportIndex=i;
                         System.out.println("You selected the airport "+ selectedAirport+"");
@@ -84,7 +89,7 @@ public class AirlineFlightRouteCalculator
                             System.out.print("Selection cancelled, ");  
                         }//confirmation process
                     }
-                    else{
+                    else{//runs arrival block of code
                         if(airportDatabase[previousAirportSelection].airportName.equals(pickedAirport)){
                             System.out.print("The selected arrival airport " + pickedAirport.toUpperCase() + " was the same as selected departure airport, ");
                             airportIndex=0;
@@ -109,13 +114,13 @@ public class AirlineFlightRouteCalculator
                 System.out.print("That was an invalid airport, check that your spelling is correct. ");
             }//if airporIndex=-1 it is known that the program did not find a valid airport to change the value of airportIndex to be >-1
         }
-        return airportIndex;
+        return airportIndex;//returns
     }//runs for departure and arrival airports
     public void returnAirports(){
-        System.out.println("Select different airports for the departure and arrival airports");
+        System.out.println("Select different airports for the departure and arrival airports");//warning notice
         System.out.println("Availiable Airports:");
-        for (int b=0;b<airportNumber;b++){
-            System.out.println(airportDatabase[b].airportName.toUpperCase());//converts airport names to upper case format
+        for (int b=0;b<airportNumber;b++){//runs for the amount of airports created
+            System.out.println(airportDatabase[b].airportName.toUpperCase());//prints out list of airport names and converts airport names to upper case format
         }
     }//returns all airport names within the airportDatabase array
 
--- Airport.java
+++ Airport.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.Arrays;//importing array usability (unused)
 /**
  * Class containing information about airports and methods to calculate route distance, flight time and a constructor for Airport type
  * 
@@ -7,72 +7,54 @@ import java.util.Arrays;
  */
 public class Airport
 {
-    String airportName;
-    double latitude;
-    double longitude;
-    double timeInHours;
-    double timeInMinutes;
-    private double aircraftVelocityFactor = 8.6; //should be capitalised but kept lower case due to possibility of changing variable later
-    public Airport(String airportName, double lat, double lon){
+    String airportName;//the placeholder for the name of the airport within each array
+    double latitude;//the placeholder for the latitude position of the airport within each array
+    double longitude;//the placeholder for the longitude position of the airport within each array
+    double timeInHours;//the placeholder for the time in hours 
+    double timeInMinutes;//the placeholder for the time in minutes
+    private static double AircraftVelocityFactor = 8.6;//factor to convert the distance of a route into time lengths
+    public Airport(String airportName/**/, double lat, double lon){
         this.airportName=airportName;
         this.latitude=lat;
         this.longitude=lon;
     }
 
     public double straightLineDistance(Airport destination,boolean statementShouldRun){
-        double tempDepLongitude=0;
-        double tempArrLongitude=0;
-        double latD=0;
-        double lonD=0;
+        double tempDepLongitude=0;//temporary variable rather than altering the master variable for departure longitude
+        double tempArrLongitude=0;//temporary variable rather than altering the master variable for arrival longitude
+        double latD=0;//
+        double lonD=0;//
         if(true){
             if(this.longitude<0){
-                //System.out.println("**Departure Longitude was less than 0");
                 if(destination.longitude>0){
-                    //System.out.println("Arrival Longitude was greater than 0");
-                    //System.out.println(this.longitude);
-                    //System.out.println(destination.longitude);
                     tempDepLongitude=this.longitude+180;
                     tempArrLongitude=destination.longitude-180;
-                    //System.out.println(tempDepLongitude);
-                    //System.out.println(tempArrLongitude);
                 }
             }else{
-                //System.out.println("Departure Longitude was greater than 0");
                 if(destination.longitude<0){
-                    //System.out.println("Arrival Longitude was less than 0");
-                    //System.out.println(this.longitude);
-                    //System.out.println(destination.longitude);
                     tempDepLongitude=this.longitude-180;
                     tempArrLongitude=destination.longitude+180;
-                    //System.out.println(this.longitude);
-                    //System.out.println(destination.longitude);
                 }
             }
             latD=this.latitude-destination.latitude;
             lonD=tempDepLongitude-tempArrLongitude;
         }
         else{
-
             latD=this.latitude-destination.latitude;
             lonD=this.longitude-destination.longitude;
         }
-        double straightDistance=Math.sqrt(Math.pow(latD,2)+Math.pow(lonD,2));
-        /*double timeInHours;
-        double timeInMinutes;
-        timeInHours=straightDistance/aircraftVelocityFactor;
-        timeInMinutes=60*(straightDistance/aircraftVelocityFactor);*/
-
+        double straightDistance=Math.sqrt(Math.pow(latD,2)+Math.pow(lonD,2));//pythagoras to calculate hypotenuse of triangle created with sides of latitude and longitude
         return straightDistance;
     }//calculates and returns straight line distance between departure and arrival airports
 
     public double flightTimeInHours(Airport destination){
         double timeInHours;
-        timeInHours=this.straightLineDistance(destination,false)/aircraftVelocityFactor;
+        timeInHours=this.straightLineDistance(destination,false)/AircraftVelocityFactor;
         return timeInHours;
     }//calculates and returns the time in hours that a flight takes to complete
     public double flightTimeInMinutes(Airport destination){
         double timeInMinutes;
-        timeInMinutes=60*(this.straightLineDistance(destination,false)/aircraftVelocityFactor);
+        timeInMinutes=60*(this.straightLineDistance(destination,false)/AircraftVelocityFactor);
         return timeInMinutes;
     }//calculates and returns the time in minutes that a flight takes to complete
 
